aa72f97d95d43a990c026488adf818b35a9a2924
Matheus Ricardo Parreira da Silva <matheus.p.parreira.s@gmail.com>
2025-06-05 23:36:14 -0300

    FEAT comentarios guia

--- postcss.config.js
+++ postcss.config.js
@@ -4,3 +4,7 @@ module.exports = {
     autoprefixer: {},
   },
 }
+
+// - Filip Arnhold Outa — RM: 559294
+// - Marcos Eduardo Hideyoshi Azuma — RM: 559883
+// - Matheus Ricardo Parreira da Silva — RM: 560099
