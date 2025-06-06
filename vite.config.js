083f5049699e21e816a40e9b23b5c32f32934421
Matheus Ricardo Parreira da Silva <matheus.p.parreira.s@gmail.com>
2025-06-05 23:35:13 -0300

    FEAT linhas de codigo

--- vite.config.js
+++ vite.config.js
@@ -13,3 +13,7 @@ export default defineConfig({
     sourcemap: true
   }
 })
+
+// - Filip Arnhold Outa — RM: 559294
+// - Marcos Eduardo Hideyoshi Azuma — RM: 559883
+// - Matheus Ricardo Parreira da Silva — RM: 560099
